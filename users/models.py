524bc315aff7260996c5b3d7b55fb6cbc3b62f9e
sangahnhan <sangahn0524@gmail.com>
2022-03-24 15:45:08 +0900

    Add: users 앱 User 모델링, cores 앱 TimeStampModel 모델링

---
+++ users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from cores.models import TimeStampModel
+
+class User(TimeStampModel):
+    name         = models.CharField(max_length=100)
+    email        = models.CharField(max_length=100 , unique=True)
+    password     = models.CharField(max_length=200)
+    phone_number = models.IntegerField()
+    etc_info     = models.CharField(max_length=500 , null=True)
+
+    class Meta:
+        db_table = 'users'

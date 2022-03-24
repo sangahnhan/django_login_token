ad3563b7f999030c2569bc95ac424a8bed3376e3
sangahnhan <sangahn0524@gmail.com>
2022-03-24 18:05:31 +0900

    Add: 회원가입 구현

--- users/views.py
+++ users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views           import View
+from django.http            import JsonResponse
+from django.core.exceptions import ValidationError
+
+from users.models    import User
+from cores.validator import *
+
+class SignUpView(View):
+    def post(self,request):
+        try:
+            data         = json.loads(request.body)
+            name         = data['name']
+            email        = data['email']
+            password     = data['password']
+            phone_number = data['phone_number']
+            etc_info     = data['etc_info']
+
+            validate_email(email)
+            check_email_duplication(email)
+            validate_password(password)
+
+            User.objects.create(
+                    name         = name,    
+                    email        = email,
+                    password     = password,
+                    phone_number = phone_number,
+                    etc_info     = etc_info
+            )
+            return JsonResponse({'message' : 'SUCCESS'} , status=201)
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"} , status=400)
+        except ValidationError as e:
+            return JsonResponse({'message' : e.message} , status=400)

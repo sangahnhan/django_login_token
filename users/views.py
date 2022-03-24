43c60ce6464dad17197e2a068dd536edfcceaa04
sangahnhan <sangahn0524@gmail.com>
2022-03-24 19:02:54 +0900

    Add: 비밀번호 암호화

--- users/views.py
+++ users/views.py
@@ -21,15 +21,30 @@ class SignUpView(View):
             check_email_duplication(email)
             validate_password(password)
 
+            password_encrypt = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
             User.objects.create(
                     name         = name,    
                     email        = email,
-                    password     = password,
+                    password     = password_encrypt,
                     phone_number = phone_number,
                     etc_info     = etc_info
             )
             return JsonResponse({'message' : 'SUCCESS'} , status=201)
         except KeyError:
-            return JsonResponse({"message" : "KEY_ERROR"} , status=400)
+            return JsonResponse({'message' : 'KEY_ERROR'} , status=400)
         except ValidationError as e:
             return JsonResponse({'message' : e.message} , status=400)
+
+class SignInView(View):
+    def post(self,request):
+        try:
+            data     = json.loads(request.body)
+            email    = data['email']
+            password = data['password']
+
+            if not User.objects.filter(email = email, password = password).exsists(): 
+                return JsonResponse({'message' : 'INVALID_USER'}, status=401)
+            return JsonResponse({'message' : 'LOGIN SUCCESS'}, status=200)
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"} , status=400)
